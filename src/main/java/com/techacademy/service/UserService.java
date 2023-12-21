ee57e4fcca334f05a6ecfedec9e687e7de9ba893
19kyosuke37 <19kyosuke37@gmail.com>
2023-12-22 00:11:32 +0900

    chapter９まで 難しい。。。

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,8 +1,10 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -17,9 +19,30 @@ public class UserService {
 
     }
 
-    public List<User> getUserList(){
+    public List<User> getUserList(){ //List<User> これは下のreturnの戻り値。intやStringと同じ。
 
         return userRepository.findAll();
     }
+
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for (Integer id : idck) {
+            userRepository.deleteById(id);
+        }
+
+    }
+
+
+
 
 }
